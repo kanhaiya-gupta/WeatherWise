1850e0dee4c3a8a5ee07dc88f27df45c954295ce
Kanhaiya Gupta <kanhaiya.lgupta21@gmail.com>
2025-04-12 17:02:14 +0200

    tests

--- tests/test_model.py
+++ tests/test_model.py
@@ -1,9 +1,2 @@
 def test_dummy():
     assert 1 + 1 == 2
-# from src.models.model import test_model
-
-# def test_model_definition():
-#     """Test model definition."""
-#     model = test_model()
-#     assert model is not None, "Model should be defined"
-#     assert hasattr(model, 'fit'), "Model should have a fit method"
